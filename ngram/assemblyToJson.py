92f81bf259c2fde0146435a608c992f7dd583bae
tskubo12 <treetsk@gmail.com>
2019-11-06 21:37:08 +0900

    ニーモニックの一部が出力されない不具合を修正

--- ngram/assemblyToJson.py
+++ ngram/assemblyToJson.py
@@ -22,16 +22,13 @@ import os
 import subprocess
 import sys
 
+
 # +
 # 全体的な処理の流れ
 # マルウェア全ファイルの逆アセンブル結果を「assemblyTxt」に保存
 # 「assemblyTxt」内のファイルをすべてパースしワードリストを作成する
 # ワードリストをもとに各マルウェアの特徴量の抽出を行う
-# -
-
-
-
-# +
+# + {}
 def main():
     allWords =[]
     parser = argparse.ArgumentParser()
@@ -39,23 +36,98 @@ def main():
     malDir = '../byteFiles/'
 # 実行時は'~$assemblyToJson malwareDir'   
     args = parser.parse_args(args=[malDir])
-    dirName = 'assemblyTxt'
+    dirName = 'assemblyTxt/'
+    
     makeDir(dirName)
     
     for dirpath,dirnames,filenames in os.walk(malDir):
         for filename in filenames:
             if(filename.endswith('.exe')):
-                assembly = makeAssemblyTxt(malDir+filename,filename,dirName)
+                assembly = reverseAssembly(malDir+filename,dirName)
+                writeJson(assembly , dirName + filename.strip('.exe') + '.json')
                 
 #                 getAllWords(allWords,filename)
-        
 main()
 
 
 # -
 
+# objdumpで逆アセンブルを行い結果をパースし、jsonを返す
+def reverseAssembly(filePath,dirName):
+    
+    cmd = ['objdump','-d','--no-show-raw-insn',filePath]
+    assembly = subprocess.run(cmd,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+    retJson= getMalJson(filePath,assembly.stdout.decode('utf8'))
+    
+    return retJson
+
+
+
+# +
+#逆アセンブルの結果をパースしニーモニックをjsonとして返す
+def getMalJson(filePath,assembly):
+    
+    lines = assembly.split('\n')
+    lines.append(' ')
+    
+    mnemonics = []
+    results = {}
+    section = {}
+    minDicList = []
+    block = ''
+    counter = 0
+    isInSectionflag = False
+    isInBlockflag = False
+    sectionName = ''
+    
+    results['filePath'] = filePath
+    
+    for line in lines:
+        line = line.split('#')[0].strip('\n')#コメント削除
+        
+        if not line:
+            if(isInBlockflag and isInSectionflag ):
+                minDic = {block:mnemonics}
+                minDicList.append(minDic)
+                mnemonics = []
+                isInBlockflag = False
+                continue
+            else:
+                continue           
+        
+        
+        if(line[len(line) - 1] == ':'):
+            if('>:' in line):
+                block = line.strip(':').split()[1]
+                isInBlockflag = True
+            else:
+#             elif('.' in line.split()[1]):
+                if sectionName:
+                    section.update({sectionName:minDicList})
+                    
+                sectionName = line.split()[1]
+                minDicList = []
+                isInSectionflag = True
+        elif(isInBlockflag == True):
+#             ニーモニックのリストを作成
+            words = line.split()
+            if(len(words) >= 2):
+                mnemonics.append(words[1])
+
+    section.update({sectionName:minDicList})
+            
+    results['Section']=section
+    
+    return results
+
+
+#     print(json.dumps(results,indent = 4))
+    
+# -
+
+# ディレクトリの生成
 def makeDir(dirName):
-#     ディレクトリの生成
     try:
         if(not(os.path.exists(dirName))):
             mkdirArg = ['mkdir',dirName]
@@ -66,35 +138,12 @@ def makeDir(dirName):
         sys.exit('can\'t make directory')
 
 
-# objdumpで逆アセンブルを行い結果を'assemblyTxt/FILENAME.txt'に出力
-def makeAssemblyTxt(filePath,filename,dirName):
-    cmd = ['objdump','-d','--no-show-raw-insn',filePath]
-    assembly = subprocess.run(cmd,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-
-    ret = getMalDict(filename,assembly.stdout.decode('utf8'))
-    print(ret)
-#     assembly = False
-#     try:
-#         assembly = subprocess.run(cmd,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        
-#         ret = getMalDict(filename,assembly.stdout.decode('utf8'))
-#         print(ret)
-# #         with open(dirName + '/' + filename.split('.')[0] + '.txt' , mode='w') as f:
-# #             f.write(assembly.stdout.decode('utf8'))
-#     except:
-#         print(filePath + ' can not reverse assembly')
-    
-    return assembly
-    
-
-
 def getAllWords(allWords,fileName):
     dict = getMalDict(fileName)
     print(dict.keys)
 
 
-def 
-
+# +
 
 def generateNgramCount(dict,n,allWords):
     section = dict['Section']
@@ -109,64 +158,16 @@ def generateNgramCount(dict,n,allWords):
                 makeDictionary(ngramList)
 
 
-def writeAsm(targetFile,str,mode):
-    with open(targetFile,mode) as f:
-        f.write(str)
+# -
 
-
-def getMalDict(fileName,assembly):
-    lines = assembly.split('\n')
-    writeFileName = fileName + '.json'
-    mnemonics = []
-    results = {}
-    section = {}
-    minDicList = []
-    block = ''
-    counter = 0
-    flag = 0
-    sectionName = ''
-    for line in lines:
-        if counter == 1:
-            results['fileName']=line.split(':')[0]
-            writeAsm(writeFileName,line,'w')
-            counter+=1
-            continue
-        words = line.split('#')
-#         line = line.strip('\n')
-        line = words[0] #コメント削除
-        line = line.strip('\n')
-        if not line:
-            counter+=1
-            continue
-
-        if(line[len(line) - 1] == ':'): #末尾が':'
-            if len(mnemonics) > 0:
-                minDic = {block:mnemonics}
-                minDicList.append(minDic)
-                mnemonics = []
-            if('>:' in line) :
-                block = line.strip(':')
-                flag = 1
-            else: #セクションの終わりを検知
-                words = line.split()
-                if sectionName:
-                    section.update({sectionName:minDicList})
-                minDicList = []
-                sectionName = words[1]
-                flag = 0
-        elif(flag == 1):
-#             ニーモニックのリストを作成
-            words = line.split()
-            if(len(words) >= 2):
-                mnemonics.append(words[1])
-            if counter == len(lines) - 1: #ファイルの末尾になった場合(最終セクションの処理)
-                minDic = {block:mnemonics}
-                minDicList.append(minDic)
-                section.update({sectionName:minDicList})
-        counter+=1
-    results['Section']=section
-    return results
-
+# jsonファイルを指定したファイルに保存する
+def writeJson(assemblyJson,filePath):
+    with open(filePath,'w') as f:
+        try:
+            json.dump(assemblyJson,f,indent = 4)
+            print('complete : writing {}'.format(filePath))
+        except:
+            print('can\'t output {}'.format(filePath))
 
 
 # ngramList:ngram分割された単語、タプルのリスト
@@ -176,8 +177,64 @@ def makeDictionary(ngramList):
     for word in allWords:
         count += ngramList.count(word)
         print('{}:{}'.format(word,ngramList.count(word)))
-        
 
+# +
+# def getMalDict(fileName,assembly):
+    
+#     lines = assembly.split('\n')
+    
+    
+#     writeFileName = fileName + '.json'
+#     mnemonics = []
+#     results = {}
+#     section = {}
+#     minDicList = []
+#     block = ''
+#     counter = 0
+#     flag = 0
+#     sectionName = ''
+    
+#     results['f']
+    
+#     for line in lines:
+#         if counter == 1:#ファイル名を記録
+#             results['fileName']=line.split(':')[0]
+#             counter+=1
+#             continue
+#         line = line.split('#')[0].strip('\n')#コメント削除
+#         if not line:
+#             counter+=1
+#             continue
+
+#         if(line[len(line) - 1] == ':'): #末尾が':'
+#             if len(mnemonics) > 0:
+#                 minDic = {block:mnemonics}
+#                 minDicList.append(minDic)
+#                 mnemonics = []
+#             if('>:' in line) :
+#                 block = line.strip(':')
+#                 flag = 1
+#             else: #セクションの終わりを検知
+#                 words = line.split()
+#                 if sectionName:
+#                     section.update({sectionName:minDicList})
+#                 minDicList = []
+#                 sectionName = words[1]
+#                 flag = 0
+#         elif(flag == 1):
+# #             ニーモニックのリストを作成
+#             words = line.split()
+#             if(len(words) >= 2):
+#                 mnemonics.append(words[1])
+#             if counter == len(lines) - 1: #ファイルの末尾になった場合(最終セクションの処理)gg
+#                 minDic = {block:mnemonics}
+#                 minDicList.append(minDic)
+#                 section.update({sectionName:minDicList})
+#         counter+=1
+#     results['Section']=section
+# #     qprint(results)
+#     return results
+# -
 
 
 
